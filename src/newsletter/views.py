7212fedbf806fe2c898e81f0e9f28a976ceacb2e
Justin Mitchel <jmitchel3@gmail.com>
2015-07-13 15:46:50 -0400

    9 - Question Queryset

--- src/newsletter/views.py
+++ src/newsletter/views.py
@@ -2,6 +2,8 @@ from django.conf import settings
 from django.core.mail import send_mail
 from django.shortcuts import render
 
+from questions.models import Question
+
 from .forms import ContactForm, SignUpForm
 from .models import SignUp
 
@@ -14,34 +16,23 @@ def home(request):
 		"form": form
 	}
 	if form.is_valid():
-		#form.save()
-		#print request.POST['email'] #not recommended
 		instance = form.save(commit=False)
 
 		full_name = form.cleaned_data.get("full_name")
 		if not full_name:
 			full_name = "New full name"
 		instance.full_name = full_name
-		# if not instance.full_name:
-		# 	instance.full_name = "Justin"
 		instance.save()
 		context = {
 			"title": "Thank you"
 		}
 
-	if request.user.is_authenticated() and request.user.is_staff:
-		#print(SignUp.objects.all())
-		# i = 1
-		# for instance in SignUp.objects.all():
-		# 	print(i)
-		# 	print(instance.full_name)
-		# 	i += 1
-
-		queryset = SignUp.objects.all().order_by('-timestamp') #.filter(full_name__iexact="Justin")
-		#print(SignUp.objects.all().order_by('-timestamp').filter(full_name__iexact="Justin").count())
+	if request.user.is_authenticated():
+		queryset = Question.objects.all().order_by('-timestamp') 
 		context = {
 			"queryset": queryset
 		}
+		return render(request, "questions/home.html", context)
 
 	return render(request, "home.html", context)
 
