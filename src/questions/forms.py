60ef6287845ba19ae4792bdd22bb035186f68a21
Justin Mitchel <jmitchel3@gmail.com>
2015-07-13 19:30:45 -0400

    15 - Save User Answer

--- src/questions/forms.py
+++ src/questions/forms.py
@@ -6,5 +6,5 @@ class UserResponseForm(forms.Form):
 	question_id = forms.IntegerField()
 	answer_id = forms.IntegerField()
 	importance_level = forms.ChoiceField(choices=LEVELS)
-	coworker_answer_id = forms.IntegerField()
-	coworker_importance_level = forms.ChoiceField(choices=LEVELS)
+	their_answer_id = forms.IntegerField()
+	their_importance_level = forms.ChoiceField(choices=LEVELS)
--- src/questions/views.py
+++ src/questions/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 # Create your views here.
 
 from .forms import UserResponseForm
-from .models import Question, Answer
+from .models import Question, Answer, UserAnswer
 
 
 
@@ -14,11 +14,32 @@ def single(request, id):
 		if form.is_valid():
 			print form.cleaned_data
 			#print request.POST
+
 			question_id = form.cleaned_data.get('question_id') #form.cleaned_data['question_id']
+			
 			answer_id = form.cleaned_data.get('answer_id')
+			importance_level = form.cleaned_data.get('importance_level')
+
+			their_importance_level = form.cleaned_data.get('their_importance_level')
+			their_answer_id = form.cleaned_data.get('their_answer_id')
+
+			
 			question_instance = Question.objects.get(id=question_id)
 			answer_instance = Answer.objects.get(id=answer_id)
-			print answer_instance.text, question_instance.text
+			
+			new_user_answer = UserAnswer()
+			new_user_answer.user = request.user
+			new_user_answer.question = question_instance
+			new_user_answer.my_answer = answer_instance
+			new_user_answer.my_answer_importance = importance_level
+			if their_answer_id != -1:
+				their_answer_istance = Answer.objects.get(id=their_answer_id)
+				new_user_answer.their_answer = their_answer_istance
+				new_user_answer.their_importance = their_importance_level
+			else:
+				new_user_answer.their_importance = "Not Important"
+			new_user_answer.save()
+
 			next_q = Question.objects.all().order_by("?").first()
 			return redirect("question_single", id=next_q.id)
 
