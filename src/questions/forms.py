f66167baa4617e056171769f0ab9fcf73fb79bef
Justin Mitchel <jmitchel3@gmail.com>
2015-07-13 19:36:30 -0400

    16 - Form Validation

--- src/questions/forms.py
+++ src/questions/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from .models import LEVELS
+from .models import LEVELS, Answer, Question
 
 class UserResponseForm(forms.Form):
 	question_id = forms.IntegerField()
@@ -8,3 +8,30 @@ class UserResponseForm(forms.Form):
 	importance_level = forms.ChoiceField(choices=LEVELS)
 	their_answer_id = forms.IntegerField()
 	their_importance_level = forms.ChoiceField(choices=LEVELS)
+
+
+	def clean_question_id(self):
+		question_id = self.cleaned_data.get('question_id')
+		try:
+			obj = Question.objects.get(id=question_id)
+		except:
+			raise forms.ValidationError('There was an error with the question. Please try again.')
+		return question_id	
+
+
+	def clean_answer_id(self):
+		answer_id = self.cleaned_data.get('answer_id')
+		try:
+			obj = Answer.objects.get(id=answer_id)
+		except:
+			raise forms.ValidationError('There was an error with the answer. Please try again.')
+		return answer_id
+
+
+	def clean_their_answer_id(self):
+		their_answer_id = self.cleaned_data.get('their_answer_id')
+		try:
+			obj = Answer.objects.get(id=their_answer_id)
+		except:
+			raise forms.ValidationError('There was an error with the answer you provided for them. Please try again.')
+		return their_answer_id
