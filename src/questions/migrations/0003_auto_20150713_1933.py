3386ffc22c5409be710ef9aae19f302d2188622d
Justin Mitchel <jmitchel3@gmail.com>
2015-07-13 15:38:31 -0400

    8 - Foreign Key Basics Part 2

---
+++ src/questions/migrations/0003_auto_20150713_1933.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('questions', '0002_answer'),
+    ]
+
+    operations = [
+        migrations.RemoveField(
+            model_name='answer',
+            name='question',
+        ),
+        migrations.AddField(
+            model_name='question',
+            name='answers',
+            field=models.ManyToManyField(to='questions.Answer'),
+        ),
+    ]
---
+++ src/questions/migrations/0004_auto_20150713_1937.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('questions', '0003_auto_20150713_1933'),
+    ]
+
+    operations = [
+        migrations.RemoveField(
+            model_name='question',
+            name='answers',
+        ),
+        migrations.AddField(
+            model_name='answer',
+            name='question',
+            field=models.ForeignKey(default=1, to='questions.Question'),
+            preserve_default=False,
+        ),
+    ]
