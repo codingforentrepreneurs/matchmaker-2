0aa8b0fd5dcab7b3152bd40cf65b997e058af64a
Justin Mitchel <jmitchel3@gmail.com>
2015-07-13 15:32:24 -0400

    7 - Tabular Inlines

--- src/questions/admin.py
+++ src/questions/admin.py
@@ -4,6 +4,17 @@ from django.contrib import admin
 
 from .models import Question, Answer
 
-admin.site.register(Question)
+
+class AnswerTabularInline(admin.TabularInline):
+	model = Answer
+
+class QuestionAdmin(admin.ModelAdmin):
+	inlines = [AnswerTabularInline]
+	class Meta:
+		model = Question
+
+admin.site.register(Question, QuestionAdmin)
+
+
 
 admin.site.register(Answer)
