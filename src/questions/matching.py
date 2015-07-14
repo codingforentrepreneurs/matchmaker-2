db94bbe88deeeeb0846ac5c46f5dce1efb9aa62f
Justin Mitchel <jmitchel3@gmail.com>
2015-07-14 17:06:29 -0400

    21 - Matching Algorithm Part 2 - Awarding Points

--- src/questions/matching.py
+++ src/questions/matching.py
@@ -49,12 +49,18 @@ def get_match(user_a, user_b):
 		user_a_pref = user_a_answers.their_answer
 		user_b_answer = user_b_answers.my_answer
 		user_b_pref = user_b_answers.their_answer
+		user_a_total_awarded = 0
+		user_b_total_awarded = 0
 		if user_a_answer == user_b_pref:
+			user_b_total_awarded += user_b_answers.their_points
 			print "%s fits with %s's preference" %(user_a_answers.user.username, user_b_answers.user.username)
 		if user_a_pref == user_b_answer:
+			user_a_total_awarded += user_a_answers.their_points
 			print "%s fits with %s's preference" %(user_a_answers.user.username, user_b_answers.user.username)
 		if user_a_answer == user_b_pref and user_a_pref == user_b_answer:
 			print "this is an ideal answer for both"
+		print user_a, user_a_total_awarded, user_b
+		print user_b, user_b_total_awarded, user_a
 
 
 
