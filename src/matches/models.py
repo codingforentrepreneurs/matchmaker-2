f1871f926f4dd1944787bc3bddd8b5c6332dfe86
Justin Mitchel <jmitchel3@gmail.com>
2015-07-15 19:23:38 -0400

    26 - Model Manager for Matches

--- src/matches/models.py
+++ src/matches/models.py
@@ -3,6 +3,29 @@ from django.db import models
 
 # Create your models here.
 
+class MatchManager(models.Manager):
+	def get_or_create_match(self, user_a=None, user_b=None):
+		try:
+			obj = self.get(user_a=user_a, user_b=user_b)
+		except:
+			obj = None
+		try:
+			obj_2 = self.get(user_a=user_b, user_b=user_a)
+		except:
+			obj_2 = None
+		if obj and not obj_2:
+			return obj, False
+		elif not obj and obj_2:
+			return obj_2, False
+		else:
+			new_instance = self.create(user_a=user_a, user_b=user_b)
+			#add match %
+			new_instance.match_decimal = 0.85
+			new_instance.questions_answered = 20
+			new_instance.save()
+			return new_instance, True
+			
+
 
 class Match(models.Model):
 	user_a = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='match_user_a')
@@ -13,8 +36,30 @@ class Match(models.Model):
 	updated = models.DateTimeField(auto_now_add=False, auto_now=True)
 
 	def __unicode__(self): #__str__(self)
-		return self.match_decimal
+		return "%.2f" %(self.match_decimal)
+
+	objects = MatchManager()
 
 	#good match?
 	#percentage value?
 	
+
+
+
+
+'''
+Match.abc.get_or_create_match()
+Match.objects.get_or_create_match()
+
+
+Match.objects.all()
+Match.objects.get(user_a=some_user)
+instance, created = Match.objects.get_or_create(user_a=some_user)
+new_instance = Match.objects.create(user_a=some_user)
+
+
+new_instance = Match()
+new_instance.user_a = some_user
+new_instance.save()
+
+'''
