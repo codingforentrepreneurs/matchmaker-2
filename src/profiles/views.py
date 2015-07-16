b2b971aefb4bc0726baa5bfda5b9356f4a63d149
Justin Mitchel <jmitchel3@gmail.com>
2015-07-16 18:45:04 -0400

    34 - Implementing Matching Function

--- src/profiles/views.py
+++ src/profiles/views.py
@@ -1,13 +1,22 @@
 from django.contrib.auth import get_user_model
+from django.contrib.auth.decorators import login_required
+from django.http import Http404
 from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 
 User = get_user_model()
+
+from matches.models import Match
 from .models import Profile
 
+@login_required
 def profile_view(request, username):
 	user = get_object_or_404(User, username=username)
 	profile, created = Profile.objects.get_or_create(user=user)
-	context = {"profile": profile}
+	match, match_created = Match.objects.get_or_create_match(user_a=request.user, user_b=user)
+	context = {
+		"profile": profile,
+		"match": match
+				}
 	return render(request, "profiles/profile_view.html", context)
