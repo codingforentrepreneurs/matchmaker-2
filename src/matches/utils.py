e71f8828f77bf1c59a2c205ff986fe9c7c583325
Justin Mitchel <jmitchel3@gmail.com>
2015-07-16 16:39:20 -0400

    31 - Matching Algorithm Final

--- src/matches/utils.py
+++ src/matches/utils.py
@@ -4,84 +4,44 @@ from django.db.models import Q
 from questions.models import UserAnswer, Question
 
 
-# User = get_user_model()
-
-# users = User.objects.all() #[user1, user2,]
-# all_user_answers = UserAnswer.objects.all().order_by("user__id") #[useranswer1, useranswer2,]
-
-
-# jmitchel3 = users[0]
-# khaleesi = users[1]
-# userc = users[2]
-
-
-def get_points_2(user_a, user_b):
-	q1 = Q(useranswer__user=a)
-	q2 = Q(useranswer__user=b)
+def get_match(user_a, user_b):
+	q1 = Q(useranswer__user=user_a)
+	q2 = Q(useranswer__user=user_b)
 	question_set = Question.objects.filter(q1 | q2).distinct()
+	a_points = 0
+	b_points = 0  
+	a_total_points = 0
+	b_total_points = 0
+	questions_in_common = 0
 	for question in question_set:
 		try:
-			a_answer = UserAnswer.objects.get(user=user_a, question=question)
+			a = UserAnswer.objects.get(user=user_a, question=question)
 		except:
-			a_answer = None
+			a = None
 		try:
-			b_answer = UserAnswer.objects.get(user=user_b, question=question)
+			b = UserAnswer.objects.get(user=user_b, question=question)
 		except:
-			b_answer = None
-		if a_answer and b_answer:
-			print("something")
-
-
-
-
-
-def get_points(user_a, user_b):
-	a_answers = UserAnswer.objects.filter(user=user_a)
-	b_answers = UserAnswer.objects.filter(user=user_b)
-	a_total_awarded = 0
-	a_points_possible = 0
-	num_question = 0
-	for a in a_answers:
-		for b in b_answers:
-			if a.question.id == b.question.id:
-				num_question += 1
-				a_pref = a.their_answer
-				b_answer = b.my_answer
-				if a_pref == b_answer:
-					'''
-					awards points for correct answer
-					'''
-					a_total_awarded += a.their_points
-				'''
-				assiging total points
-				'''
-				a_points_possible += a.their_points
-			print "%s has awarded %s points of %s to %s" %(user_a, a_total_awarded, a_points_possible, user_b)
-	percent = a_total_awarded / Decimal(a_points_possible)
-	print percent, num_question
-	if percent == 0:
-		percent = 0.000001
-	return percent, num_question
-
-
-
-# get_points(jmitchel3, khaleesi)
-# # get_points(jmitchel3, userc)
-# a = get_points(khaleesi, jmitchel3)
-# b = get_points(jmitchel3, khaleesi)
-
-
-# match_percentage = "%.2f" % ((Decimal(a[0]) * Decimal(b[0])) ** (1/Decimal(b[1])))
-# print match_percentage
-
-def get_match(user_a, user_b):
-	a = get_points(user_a, user_b)
-	b = get_points(user_b, user_a)
-	#a[0] = decimal match value 
-	number_of_questions = b[1] #b[1]/a[1] = number of questions answered
-	match_decimal = (Decimal(a[0]) * Decimal(b[0])) ** (1/Decimal(number_of_questions)) #geometric mean in respects to number of questions answered
-	return match_decimal, number_of_questions
-
+			b = None
+		if a and b:
+			questions_in_common += 1
+			if a.their_answer == b.my_answer:
+				b_points += a.their_points
+			b_total_points += a.their_points
+			if b.their_answer == a.my_answer:
+				a_points += b.their_points
+			a_total_points += b.their_points
+	if questions_in_common > 0:
+		a_decimal = a_points / Decimal(a_total_points)
+		b_decimal = b_points / Decimal(b_total_points)
+		print b_decimal, a_decimal
+		if a_decimal == 0:
+			a_decimal = 0.000001
+		if b_decimal == 0:
+			b_decimal = 0.000001
+		match_percentage = (Decimal(a_decimal) * Decimal(b_decimal)) ** (1/Decimal(questions_in_common))
+		return match_percentage, questions_in_common
+	else:
+		return 0.0, 0
 
 
 
