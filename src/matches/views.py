bdf62696769e0c562574842b822082a64e7ebf2f
Justin Mitchel <jmitchel3@gmail.com>
2015-07-21 15:46:09 -0400

    53 - Handle Query Exceptions

--- src/matches/views.py
+++ src/matches/views.py
@@ -1,10 +1,18 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from jobs.models import Job, Employer, Location
 
 def position_match_view(request, slug):
-	instance = Job.objects.get(slug=slug)
+	try:
+		instance = Job.objects.get(slug=slug)
+	except Job.MultipleObjectsReturned:
+		queryset = Job.objects.filter(slug=slug).order_by('-id')
+		instance = queryset[0]
+	except Job.DoesNotExist:
+		raise Http404
+
 	template = "matches/position_match_view.html"
 	context = {
 		"instance": instance,
