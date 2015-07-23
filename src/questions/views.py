270caf5b00cee10e6420da40d4f4417df25133a0
Justin Mitchel <jmitchel3@gmail.com>
2015-07-23 00:30:59 -0400

    63 - Unanswered Questions Part 2

--- src/questions/views.py
+++ src/questions/views.py
@@ -54,9 +54,12 @@ def single(request, id):
 				user_answer.their_importance = "Not Important"
 			user_answer.save()
 
-			next_q = Question.objects.all().order_by("?").first()
-			return redirect("question_single", id=next_q.id)
-
+			next_q = Question.objects.get_unanswered(request.user).order_by("?")
+			if next_q.count() > 0:
+				next_q_instance = next_q.first()
+				return redirect("question_single", id=next_q_instance.id)
+			else:
+				return redirect("home")
 
 		context = {
 			"form": form,
