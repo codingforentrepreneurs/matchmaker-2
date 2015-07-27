a996925deaef0fe91639be0e2c0aebb4bf89f439
Justin Mitchel <jmitchel3@gmail.com>
2015-07-27 16:33:14 -0400

    update get_user_matches_receiver

--- src/matches/views.py
+++ src/matches/views.py
@@ -1,10 +1,22 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.signals import user_logged_in
+from django.dispatch import receiver
 from django.http import Http404
 from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from jobs.models import Job, Employer, Location
+from profiles.models import Profile
+from .models import PositionMatch, LocationMatch, EmployerMatch, Match
 
-from .models import PositionMatch, LocationMatch, EmployerMatch
+User = get_user_model()
+
+@receiver(user_logged_in)
+def get_user_matches_receiver(sender, request, user, *args, **kwargs):
+	for u in User.objects.exclude(username=user.username).order_by("-id")[:200]:
+		profile = Profile.objects.get_or_create(user=u)
+		matched, created = Match.objects.get_or_create_match(user_a=u,user_b=user)
+
 
 def position_match_view(request, slug):
 	try:
